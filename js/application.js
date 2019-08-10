9a73016b751fcda417986a14061ed40da1be36ba
dwddavis <50349984+dwddavis@users.noreply.github.com>
2019-08-09 21:25:58 -0500

    updated timer, updated score, and css.

--- js/application.js
+++ js/application.js
@@ -1,20 +1,29 @@
 var scoring = 0;
+var timex = 10;
 
 var mathGen = function(){
-  var rightAnswer=0;
-  var first = Math.floor((Math.random()*50)+1);
-  var second = Math.floor((Math.random()*(50-first))+1);
+
+  var first = Math.floor((Math.random()*10)+1);
+  var second = Math.floor((Math.random()*(10-first))+1);
   rightAnswer= first+second;
 
   $('#numbers').empty();
   $('#numbers').append(first + ' + ' + second + ' =  ');
+  $('#answer').focus();
 
 
 
-
+  var timeout;
   $('input').on('input', function(){
+    clearTimeout(timeout);
+    timeout=setTimeout(function(){
+      compare();
+    },200);
+});
 
+}
 
+var compare = function(){
     var userAnswer = parseFloat($('#answer').val());
     if( userAnswer === rightAnswer){
 
@@ -22,30 +31,31 @@ var mathGen = function(){
       $('#score').empty();
       $('#score').append(scoring);
       $('input').val("");
-      rightAnswer=0;
+      timex+=1;
+      $('#timer').empty();
+      $('#timer').append(timex);
       mathGen();
 
   }
-});
-
-
 }
+
 
 var getReady = function(){
   var play = confirm('READY TO PLAY?');
   if (play === true){
+    $('#score').empty();
+    $('#score').append('0');
     timing();
     mathGen();
   }else{
     $('#numbers').empty();
-    $('#space').append('<button onclick="getReady()">Play</button>')
+    $('#numbers').append('<button style="height:60px" onclick="getReady()">Play</button>')
   }
 }
 
 
 var timing = function(){
-    var timex = 10;
-
+    timex=10;
     var x = setInterval(function(){
     timex -=1;
     $('#timer').empty();
@@ -56,11 +66,10 @@ var timing = function(){
       alert('gameover');
       $('#timer').empty();
       $('#timer').append('10');
+      $('input').empty();
       getReady();
     }
-
   },1000);
-
 }
 
 $(document).ready(function(){
